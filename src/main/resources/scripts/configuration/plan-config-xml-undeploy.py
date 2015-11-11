b779f67728b3486a7937c0c532f9db36329ccdba
wianvos <wvos@xebia.com>
2015-11-11 09:45:49 -0500

    intial commit

---
+++ src/main/resources/scripts/configuration/plan-config-xml-undeploy.py
@@ -0,0 +1,16 @@
+from wlp.modules.utility import Paths
+from com.xebialabs.deployit.plugin.api.deployment.specification import Operation
+from com.xebialabs.deployit.plugin.api.deployment.planning import DefaultOrders
+
+previousDeployed.location = "%s/config/%s" % (Paths.get_server_config_dir(previousDeployed.container),  previousDeployed.name)
+
+context.addStepWithCheckpoint(steps.jython(
+    description="Remove configuration entry: %s from: %s" % (previousDeployed.name, previousDeployed.container.name),
+    script="wlp/scripts/resource/destroy-resource.py",
+    order=DefaultOrders.UNDEPLOY_ARTIFACTS
+), delta, Operation.DESTROY)
+context.addStepWithCheckpoint(steps.delete(
+    description="removing imported xml file: %s from %s" % (previousDeployed.name, previousDeployed.container.name),
+    target_path=previousDeployed.location,
+    order=DefaultOrders.UNDEPLOY_ARTIFACTS + 1
+), delta, Operation.DESTROY)
